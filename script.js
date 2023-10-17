1a62b24ae173470a6e67bb9d8c8ac13e80f2e02c
itrabbi <hassan.rabbi6321@gmail.com>
2023-10-17 23:57:37 +0600

    Banner

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const navigation = document.getElementById("nav");
 const menu = document.getElementById("menu");
-menu.addEventListener("click",()=>{
+menu.addEventListener("click", () => {
     navigation.style.setProperty("--childrenNumber", navigation.children.length);
     navigation.classList.toggle("active");
     menu.classList.toggle("active");
